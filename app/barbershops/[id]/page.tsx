6b5551cb669dad993b789af9573ba0d5b6633046
TarelliEverton <tarellieverton@gmail.com>
2024-08-10 00:03:39 -0300

    feat: add phone-item component and footer

--- app/barbershops/[id]/page.tsx
+++ app/barbershops/[id]/page.tsx
@@ -1,4 +1,5 @@
 import { Button } from "@/app/_components/ui/button";
+import PhoneItem from "@/app/_components/ui/phone-item";
 import ServiceItem from "@/app/_components/ui/service-item";
 import { db } from "@/app/_lib/prisma";
 import { ChevronLeftIcon, MapPinIcon, MenuIcon, StarIcon } from "lucide-react";
@@ -71,11 +72,17 @@ const BarbershopPage =  async ({params}: BarbershopPageProps ) => {
           <p className="text-sm text-justify">{barbershop?.description}</p>
         </div>
 
-        <div className="p-5 space-y-3">
+        <div className="p-5 space-y-3 border-b border-solid">
           <h2 className="text-xs font-bold uppercase text-gray-400">Serviços</h2>
           <div className="space-y-3">
             {barbershop.services.map((service) => (<ServiceItem key={service.id} service={service} />))}
           </div>
+        </div>
+
+        <div className="p-5 space-y-3">
+          {barbershop.phones.map((phone) => (
+            <PhoneItem key={phone} phone={phone} />
+          ))}
         </div>
       </div>
   )   
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { Toaster } from "sonner";
+import { Card, CardContent } from "./_components/ui/card";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +18,20 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="dark">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {children}
+        <Toaster />
+
+        <footer>
+          <Card>
+            <CardContent className="px-5 py-6">
+              <p className="text-sm text-gray-400">
+                © 2024 Copyright <span className="font-bold">Everton Tarelli Dev</span>
+              </p>
+            </CardContent>
+          </Card>
+        </footer>  
+        </body>
     </html>
   );
 }
